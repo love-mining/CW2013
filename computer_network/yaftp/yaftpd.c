5786e22c53daefcc2cd01111ab03a5441e86a5f6
Pengyu CHEN <cpy.prefers.you@gmail.com>
2013-03-23 20:20:18 +0800

    added: init commit of yaftpd

---
+++ computer_network/yaftp/yaftpd.c
@@ -0,0 +1,11 @@
+/*
+ * This is part of coursework for Computer Network, 2013.02-2013.06
+ * By Pengyu CHEN(cpy.prefers.you@gmail.com)
+ * COPYLEFT, ALL WRONGS RESERVED.
+ * */
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
