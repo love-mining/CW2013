b23364708e86046670c38fcecfe60d437431f008
Pengyu CHEN <cpy.prefers.you@gmail.com>
2013-04-09 00:55:52 +0800

    yet shorter. linux & 32bit only.

--- computer_network/ftp64/ftp64.c
+++ computer_network/ftp64/ftp64.c
@@ -1,24 +1,22 @@
-char bbuf[0x10000],ap[14],ap2[14],*fname,*inst;
-*s[]={"RETR ","STOR ","local ","remote ","ftp32> ","succeeded.","syntax error.","remote-get %m[^\n]",
-"remote-put %m[^\n]","remote-disp %m[^\n]","open %hhu.%hhu.%hhu.%hhu:%hu","setpasv %hhu,%hhu,%hhu,%hhu,%hhu,%hhu"};
-int instfd,localfd,datafd,tmp;
-int main(int argc, char **argv){
-#define CONN(fd,ap) ({char sa[] = {2,0,ap[1],ap[0],ap[2],ap[3],ap[4],ap[5]};\
-  connect(fd = socket(2, 1, 0),sa,16);\
-  setsockopt(fd,0xffff,0x1006,&(long[]){0,1<<18},2*sizeof(long));})
-#define RECV(sockfd,outfd) ({tmp=outfd;while(write(tmp,bbuf,recv(sockfd,bbuf,0x10000,0))>0);})
-#define SEND(fd,msg) send(fd,msg,strlen(msg),0)
-#define SENDRN(fd,msg) ({SEND(fd,msg);SEND(fd,"\r\n");})
-  while(write(1,s[4],7,0),gets(bbuf)){
-    if(sscanf(bbuf,s[10],ap+2,ap+3,ap+4,ap+5,ap)==5)CONN(instfd,ap),RECV(instfd,1);
-    else if (!strncasecmp(bbuf,s[2],6))system(bbuf + 6);
-    else if (!strncasecmp(bbuf,s[3],7))SENDRN(instfd,bbuf+7),RECV(instfd,1);
-    else if (sscanf(bbuf,s[11],ap2+2,ap2+3,ap2+4,ap2+5,ap2+1,ap2)==6)puts(s[5]);
-    else if (sscanf(bbuf,s[7],&fname)==1)SEND(instfd,*s),SENDRN(instfd,fname),CONN(datafd,ap2),
-      localfd=open(fname,1|0100,0755),RECV(datafd,localfd),close(localfd),RECV(instfd,1);
-    else if (sscanf(bbuf,s[8],&fname)==1)SEND(instfd,s[1]),SENDRN(instfd,fname),CONN(datafd,ap2),
-      localfd=open(fname,0),({while(send(datafd,bbuf,read(localfd,bbuf,0x10000,0),0)>0);}),
-      close(localfd),RECV(instfd,1);
-    else if (sscanf(bbuf,s[9],&inst)==1)SENDRN(instfd,inst),CONN(datafd,ap2),RECV(datafd,1),RECV(instfd,1); 
-    else puts(s[6]);
+*s[]={"\r\n","RETR ","STOR ","local ","remote ","ftp32> ","succeeded",
+"syntax error","remote-get %m[^\n]","remote-put %m[^\n]","remote-disp %m[^\n]"
+,"open %hhu.%hhu.%hhu.%hhu:%hu","setpasv %hhu,%hhu,%hhu,%hhu,%hhu,%hhu"},
+*f,*c;char _[0x10000],*a=_+100;main(i,l,d,t,x){
+#define CONN(f,a)({char t[]={2,0,a[1],a[0],a[2],a[3],a[4],a[5]};\
+  connect(f=socket(2,1,0),t,16);setsockopt(f,1,20,&(int[]){0,1<<19},16);})
+#define RECV(s,o)({t=o;while((x=recv(s,_,0x10000,0))>0&&write(t,_,x)>0);})
+#define SEND(f,m) ({send(f,m,strlen(m),0);})
+#define SENDRN(f,m) ({SEND(f,m);SEND(f,s[0]);})
+  while(write(1,s[5],7,0),gets(_)){
+    if(sscanf(_,s[11],a+2,a+3,a+4,a+5,a)==5)CONN(i,a),RECV(i,1);
+    else if (!strncasecmp(_,s[3],6))system(_ + 6);
+    else if (!strncasecmp(_,s[4],7))SENDRN(i,_+7),RECV(i,1);
+    else if (sscanf(_,s[12],a+2,a+3,a+4,a+5,a+1,a)==6)puts(s[6]);
+    else if (sscanf(_,s[8],&f)==1)SEND(i,*s),SENDRN(i,f),CONN(d,a),
+      l=open(f,1|0100,0755),RECV(d,l),close(l),RECV(i,1);
+    else if (sscanf(_,s[9],&f)==1)SEND(i,s[2]),SENDRN(i,f),CONN(d,a),
+      l=open(f,0),({while(send(d,_,read(l,_,0x10000,0),0)>0);}),
+      close(l),RECV(i,1);
+    else if (sscanf(_,s[10],&c)==1)SENDRN(i,c),CONN(d,a),RECV(d,1),RECV(i,1); 
+    else puts(s[7]);
 }return;}
