23cbc69f62299ffac39821ed5fb5e8477239014e
Pengyu CHEN <cpy.prefers.you@gmail.com>
2013-04-08 22:38:53 +0800

    well some progress

--- computer_network/ftp64/ftp64.c
+++ computer_network/ftp64/ftp64.c
@@ -7,6 +7,8 @@
 #include <fcntl.h>
 #include <errno.h>
 char bbuf[0x10000],ap[14],ap2[14],*fname,*inst;
+*s[]={"RETR ","STOR ","local ","remote ","ftp32> ","succeeded.","syntax error.","remote-get %m[^\n]",
+"remote-put %m[^\n]","remote-disp %m[^\n]","open %hhu.%hhu.%hhu.%hhu:%hu","setpasv %hhu,%hhu,%hhu,%hhu,%hhu,%hhu"};
 int instfd,localfd,datafd,tmp;
 int main(int argc, char **argv){
 #define CONN(fd,ap) ({struct sockaddr sa = {AF_INET, {ap[1],ap[0],ap[2],ap[3],ap[4],ap[5]}};\
@@ -15,23 +17,16 @@ int main(int argc, char **argv){
 #define RECV(sockfd,outfd) ({tmp=outfd;while(write(tmp,bbuf,recv(sockfd,bbuf,0x10000,0))>0);})
 #define SEND(fd,msg) send(fd,msg,strlen(msg),0)
 #define SENDRN(fd,msg) ({SEND(fd,msg);SEND(fd,"\r\n");})
-  while(write(1,"ftp64> ",7,0), gets(bbuf)){
-    if(sscanf(bbuf,"open %hhu.%hhu.%hhu.%hhu:%hu",ap+2,ap+3,ap+4,ap+5,ap)==5){
-      CONN(instfd,ap);RECV(instfd,1);
-    } else if (!strncasecmp(bbuf, "local ", 6)) { system(bbuf + 6);
-    } else if (!strncasecmp(bbuf, "remote ", 7)) { SENDRN(instfd,bbuf+7); RECV(instfd,1);
-    } else if (sscanf(bbuf, "setpasv %hhu,%hhu,%hhu,%hhu,%hhu,%hhu",
-      ap2+2,ap2+3,ap2+4,ap2+5,ap2+1,ap2) == 6) { puts("succeeded.");
-    } else if (sscanf(bbuf, "remote-get %m[^\n]",&fname) == 1){
-      SEND(instfd,"RETR "); SENDRN(instfd,fname); CONN(datafd,ap2);
-      localfd=open(fname,O_WRONLY|O_CREAT,0755);RECV(datafd,localfd);
-      close(localfd); RECV(instfd,1);
-    } else if (sscanf(bbuf, "remote-put %m[^\n]",&fname) == 1){
-      SEND(instfd,"STOR "); SENDRN(instfd,fname); CONN(datafd,ap2);localfd=open(fname,O_RDONLY);
-      while(send(datafd,bbuf,read(localfd,bbuf,0x10000,0),0)>0);close(localfd); RECV(instfd,1);
-    } else if (sscanf(bbuf,"remote-disp %m[^\n]", &inst) == 1)  {
-      SENDRN(instfd,inst); CONN(datafd,ap2); RECV(datafd,1); RECV(instfd,1); 
-    } else puts("syntax error.");
-  }
-  return;
-}
+  while(write(1,s[4],7,0),gets(bbuf)){
+    if(sscanf(bbuf,s[10],ap+2,ap+3,ap+4,ap+5,ap)==5)CONN(instfd,ap),RECV(instfd,1);
+    else if (!strncasecmp(bbuf,s[2],6))system(bbuf + 6);
+    else if (!strncasecmp(bbuf,s[3],7))SENDRN(instfd,bbuf+7),RECV(instfd,1);
+    else if (sscanf(bbuf,s[11],ap2+2,ap2+3,ap2+4,ap2+5,ap2+1,ap2)==6)puts(s[5]);
+    else if (sscanf(bbuf,s[7],&fname)==1)SEND(instfd,*s),SENDRN(instfd,fname),CONN(datafd,ap2),
+      localfd=open(fname,O_WRONLY|O_CREAT,0755),RECV(datafd,localfd),close(localfd),RECV(instfd,1);
+    else if (sscanf(bbuf,s[8],&fname)==1)SEND(instfd,s[1]),SENDRN(instfd,fname),CONN(datafd,ap2),
+      localfd=open(fname,O_RDONLY),({while(send(datafd,bbuf,read(localfd,bbuf,0x10000,0),0)>0);}),
+      close(localfd),RECV(instfd,1);
+    else if (sscanf(bbuf,s[9],&inst)==1)SENDRN(instfd,inst),CONN(datafd,ap2),RECV(datafd,1),RECV(instfd,1); 
+    else puts(s[6]);
+}return;}
